dbd79dcc11288c573713df0ffd37e5bffa28bfcf
lucytaqr <lucytaqr08@gmail.com>
2026-05-05 14:54:50 +0700

    feat: my cart screen

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/controllers/theme_controller.dart';
 import 'package:ecommerce_app/view/widgets/all_products_screen.dart';
+import 'package:ecommerce_app/view/widgets/cart_screen.dart';
 import 'package:ecommerce_app/view/widgets/category_chips.dart';
 import 'package:ecommerce_app/view/widgets/custom_search_bar.dart';
 import 'package:ecommerce_app/view/widgets/product_grid.dart';
@@ -52,7 +53,7 @@ class HomeScreen extends StatelessWidget {
                   ),
                   // cart button
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () => Get.to(() => CartScreen()),
                     icon: const Icon(Icons.shopping_bag_outlined),
                   ),
                   // theme button
