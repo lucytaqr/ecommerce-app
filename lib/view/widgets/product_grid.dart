f98ebde2084c1d25392055bab79e0bc2570d6bf4
lucytaqr <lucytaqr08@gmail.com>
2026-05-05 09:23:21 +0700

    feat: product detail screen

--- lib/view/widgets/product_grid.dart
+++ lib/view/widgets/product_grid.dart
@@ -1,5 +1,7 @@
 import 'package:ecommerce_app/models/product.dart';
+import 'package:ecommerce_app/view/product_details_screen.dart';
 import 'package:ecommerce_app/view/widgets/product_card.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ProductGrid extends StatelessWidget {
@@ -19,7 +21,12 @@ class ProductGrid extends StatelessWidget {
       itemBuilder: (context, index){
         final product =  products[index];
         return GestureDetector(
-          onTap: (){},
+          onTap: ()=> Navigator.push(
+              context,
+            MaterialPageRoute(builder: (context) => ProductDetailsScreen(
+              product: product,
+            ),)
+          ),
           child: ProductCard(product: product),
         );
       },
